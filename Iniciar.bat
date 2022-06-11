55839f76f3d45bdaed517840a3b50f6878fc95db
mikkenienow <34070014+mikkenienow@users.noreply.github.com>
2022-06-11 17:34:45 -0300

    1.0

--- Iniciar.bat
+++ Iniciar.bat
@@ -1,3 +1,4 @@
+c:
 cd C:\Program Files\Virtual Desktop Streamer\
 
-VirtualDesktop.Streamer.exe "E:\_Programs\Unity Hub\Editor\2021.2.17f1\Editor\Unity.exe" -projectpath C:\Users\T-Gamer\Documents\GitHub\PrimeirosPassos\ -useHub -hubIPC -cloudEnvironment production
+VirtualDesktop.Streamer.exe "E:\_Programs\Unity Hub\Editor\2021.2.19f1\Editor\Unity.exe" -projectpath E:\_Projetos\_VRPlaces\VR-Places-UNITY\ -useHub -hubIPC -cloudEnvironment production
